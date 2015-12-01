01a5612d680f05273e13c582baab645f2be2ef18
Kareem Kouddous <kareemknyc@gmail.com>
2015-12-01 15:52:43 -0500

    Use latest Hockeyapp SDK

--- lib/motion/project/launcher.rb
+++ lib/motion/project/launcher.rb
@@ -10,13 +10,8 @@ class BITHockeyManagerLauncher
     return unless @plist
     #Retain self since BITHockeyManager keeps a weak reference and app code typically don't retain the BITHockeyManagerLauncher instance. No worry about memory leak since this instance should last for the duration of the app's lifetime.
     self.retain
-    if BITHockeyManager.sharedHockeyManager.respond_to? 'configureWithBetaIdentifier:liveIdentifier:delegate:'
-      BITHockeyManager.sharedHockeyManager.configureWithBetaIdentifier(@plist['beta_id'], liveIdentifier:@plist['live_id'], delegate:self)
-      BITHockeyManager.sharedHockeyManager.crashManager.crashManagerStatus = BITCrashManagerStatusAutoSend
-    else
-      BITHockeyManager.sharedHockeyManager.configureWithIdentifier(@plist['beta_id'], delegate:self)
-      BITHockeyManager.sharedHockeyManager.crashManager.autoSubmitCrashReport = true
-    end
+    BITHockeyManager.sharedHockeyManager.configureWithIdentifier(@plist['beta_id'], delegate:self)
+    BITHockeyManager.sharedHockeyManager.crashManager.crashManagerStatus = BITCrashManagerStatusAutoSend
     block.call if !block.nil?
     BITHockeyManager.sharedHockeyManager.startManager
     true
